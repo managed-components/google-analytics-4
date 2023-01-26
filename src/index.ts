488898a19dd782a39deef6ff6fdd7da632ce410f
Simona Badoiu <sbadoiu@cloudflare.com>
2023-01-26 13:59:50 +0000

    use post instead of get requests

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,104 @@
 import { ComponentSettings, Manager, MCEvent } from '@managed-components/types'
 import { getFinalURL } from './requestBuilder'
 
+const sendGaAudiences = (
+  event: MCEvent,
+  settings: ComponentSettings,
+  requestBody: Record<string, any>
+) => {
+  const { client } = event
+
+  const baseDoubleClick = 'https://stats.g.doubleclick.net/g/collect?'
+  const doubleClick = {
+    t: 'dc',
+    aip: '1',
+    _r: '3',
+    v: '1',
+    _v: 'j86',
+    tid: settings.tid,
+    cid: requestBody['cid'],
+    _u: 'KGDAAEADQAAAAC~',
+    z: (+Math.floor(2147483647 * Math.random())).toString(),
+  }
+  const doubleClickParams = new URLSearchParams(doubleClick).toString()
+  const finalDoubleClickURL = baseDoubleClick + doubleClickParams
+
+  if (
+    settings['ga-audiences'] &&
+    (!client.get('_z_ga_audiences') ||
+      client.get('_z_ga_audiences') !== requestBody['cid'])
+  ) {
+    // Build the GAv4 Audiences request
+    const audiences = {
+      ...doubleClick,
+      t: 'sr',
+      _r: '4',
+      slf_rd: '1',
+    }
+    const audienceParams = new URLSearchParams(audiences).toString()
+    const baseAudienceURL = 'https://www.google.com/ads/ga-audiences?'
+    const finalAudienceURL = baseAudienceURL + audienceParams
+    let clientJSAudience = ''
+    // Call GAv4-Audiences on Google.com
+    client.fetch(finalAudienceURL)
+    client.set('_z_ga_audiences', requestBody['cid'], {
+      scope: 'infinite',
+    })
+    // Trigger the DoubleClick with XHR because we need the response text - it holds the local Google domain
+    clientJSAudience += `x=new XMLHttpRequest,x.withCredentials=!0,x.open("POST","${finalDoubleClickURL}",!0),x.onreadystatechange=function(){`
+    clientJSAudience += `if (4 == x.readyState) {`
+    clientJSAudience += `const domain = x.responseText.trim();`
+    clientJSAudience += `if (domain.startsWith("1g") && domain.length > 2) {`
+    // Trigger the request to the local Google domain too
+    clientJSAudience += `fetch("${finalAudienceURL}".replace("www.google.com", "www.google."+domain.slice(2)));`
+    clientJSAudience += `}}`
+    clientJSAudience += `},x.send();`
+    client.execute(clientJSAudience)
+  } else {
+    // If no GAv4-Audiences, just trigger DoubleClick normally
+    client.fetch(finalDoubleClickURL)
+  }
+}
+
 const sendEvent = async (
   eventType: string,
   event: MCEvent,
   settings: ComponentSettings
 ) => {
   const { client } = event
-  const { finalURL, requestBody } = getFinalURL(eventType, event, settings)
+  const { requestBody } = getFinalURL(eventType, event, settings)
+
+  const baseURL = 'https://www.google-analytics.com/g/collect?'
+
+  const payload: Record<string, any> = {}
+  const queryParams = new URLSearchParams(requestBody)
+  for (const key of Object.keys(requestBody)) {
+    if (key.startsWith('ep') || key.startsWith('pr') || key === 'en') {
+      queryParams.delete(key)
+      payload[key] = requestBody[key]
+    }
+  }
+  const finalURL = baseURL + queryParams
+
+  const body = new URLSearchParams(payload).toString()
+
+  fetch(finalURL, {
+    method: 'POST',
+    headers: {
+      'Content-Length': '' + new TextEncoder().encode(body).length,
+      'User-Agent': event.client.userAgent,
+    },
+    body,
+  })
+
   fetch(finalURL, {
     headers: { 'User-Agent': client.userAgent },
   })
 
   if (settings['ga-audiences'] || settings['ga-doubleclick']) {
-    const baseDoubleClick = 'https://stats.g.doubleclick.net/g/collect?'
-    const doubleClick = {
-      t: 'dc',
-      aip: '1',
-      _r: '3',
-      v: '1',
-      _v: 'j86',
-      tid: settings.tid,
-      cid: requestBody['cid'],
-      _u: 'KGDAAEADQAAAAC~',
-      z: (+Math.floor(2147483647 * Math.random())).toString(),
-    }
-    const doubleClickParams = new URLSearchParams(doubleClick).toString()
-    const finalDoubleClickURL = baseDoubleClick + doubleClickParams
-
-    if (
-      settings['ga-audiences'] &&
-      (!client.get('_z_ga_audiences') ||
-        client.get('_z_ga_audiences') !== requestBody['cid'])
-    ) {
-      // Build the GAv4 Audiences request
-      const audiences = {
-        ...doubleClick,
-        t: 'sr',
-        _r: '4',
-        slf_rd: '1',
-      }
-      const audienceParams = new URLSearchParams(audiences).toString()
-      const baseAudienceURL = 'https://www.google.com/ads/ga-audiences?'
-      const finalAudienceURL = baseAudienceURL + audienceParams
-      let clientJSAudience = ''
-      // Call GAv4-Audiences on Google.com
-      client.fetch(finalAudienceURL)
-      client.set('_z_ga_audiences', requestBody['cid'], {
-        scope: 'infinite',
-      })
-      // Trigger the DoubleClick with XHR because we need the response text - it holds the local Google domain
-      clientJSAudience += `x=new XMLHttpRequest,x.withCredentials=!0,x.open("POST","${finalDoubleClickURL}",!0),x.onreadystatechange=function(){`
-      clientJSAudience += `if (4 == x.readyState) {`
-      clientJSAudience += `const domain = x.responseText.trim();`
-      clientJSAudience += `if (domain.startsWith("1g") && domain.length > 2) {`
-      // Trigger the request to the local Google domain too
-      clientJSAudience += `fetch("${finalAudienceURL}".replace("www.google.com", "www.google."+domain.slice(2)));`
-      clientJSAudience += `}}`
-      clientJSAudience += `},x.send();`
-      client.execute(clientJSAudience)
-    } else {
-      // If no GAv4-Audiences, just trigger DoubleClick normally
-      client.fetch(finalDoubleClickURL)
-    }
+    sendGaAudiences(event, settings, requestBody)
   }
+
   client.set('let', Date.now().toString()) // reset the last event time
 }
 
